70d192bdcc4f0421b9c48a3fd23eecc9bfa3456c
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-04 23:20:21 -0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,37 @@
+first_word = "Python"
+middle_word = "is"
+last_word = "fantastic"
+print(first_word, middle_word, last_word, sep="_", end="!\n")
+
+# Snake case is the standard for Python variables and functions. 
+# In snake case, all words are lowercase, and underscores separate words.
+
+# Separators and enders can be used to format the output of print statements.
+# The sep keyword argument specifies what should separate the values that are printed.
+# The end keyword argument specifies what should be printed at the end of the statement.
+
+country = "Brazil"
+print(type(country))
+print("I live in", country)
+country = 644
+print(type(country))
+print("I live in", country)
+country = 6.44
+print(type(country))
+print("I live in", country)
+
+#Dynamic typing in Python is a feature that allows you to reassign variables to different data types.
+
+first_name = "Felipe"
+last_name = "Ferreira"
+print(first_name + last_name)
+
+# The + operator can be used to concatenate strings.
+
+number1 = 10
+number2 = "20"
+product = number1 * number2
+print(product)
+
+# Syntax sugar is a feature of a programming language that makes it easier to write code.
+# The * operator can be used to multiply a string by an integer.
