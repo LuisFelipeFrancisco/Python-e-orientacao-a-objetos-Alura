e1c4efdde4de5d93032c790452345f60f0562273
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-04 23:55:49 -0300

    while loop

--- Projects/01-GuessingGame.py
+++ Projects/01-GuessingGame.py
@@ -3,28 +3,32 @@ print("*************** Guessing Game ***************")
 print("*********************************************")
 
 secret_number = 91
+guess_limit = 3
 
-guess = (input("Guess a number between 1 and 100: "))
+while guess_limit > 0:
+    print("You have", guess_limit, "guesses left.")
+    guess = (input("Guess a number between 1 and 100: "))
 
-right_guess = int(guess) == secret_number
-above_guess = int(guess) > secret_number
-below_guess = int(guess) < secret_number
+    right_guess = int(guess) == secret_number
+    above_guess = int(guess) > secret_number
+    below_guess = int(guess) < secret_number
 
-print("You guessed: ", guess)
+    print("You guessed: ", guess)
 
-if right_guess:
-    print("You won!")
-else:
-    if above_guess:
-        print("You were above the number!")
-    elif below_guess:
-        print("You were below the number!")
-    print("Sorry, you lost!")
+    if right_guess:
+        print("You won!")
+        print("Game over!")
+    else:
+        if above_guess:
+            print("You were above the number!")
+        elif below_guess:
+            print("You were below the number!")
 
-print("Game over!")
-    
+    guess_limit -= 1
+
 # The input function returns a string, so we need to convert it to an integer before we can compare it to the secret number.
 # The int function converts a string to an integer.
 # Intends are important in Python. 
 # The code inside the if statement is indented, and the code outside the if statement is not indented.
 # elif is short for else if.
+# The while loop will keep running as long as the condition is true.
