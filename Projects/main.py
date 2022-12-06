7ed2f2144ab2b4acd586ed979aa6d5262a952403
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-06 00:57:27 -0300

    feat: Modulating

---
+++ Projects/main.py
@@ -0,0 +1,20 @@
+import guessing_game
+import hangman_game
+
+print("*********************************************")
+print("****************** Jojinhos *****************")
+print("*********************************************")
+
+print("Select a game: ")
+print("(1) Guessing Game (2) Hangman ")
+game = int(input("Select a game: "))
+
+if game == 1:
+    print("Starting Guessing Game...")
+    guessing_game.play()
+elif game == 2:
+    print("Starting Hangman...")
+    hangman_game.play()
+    
+
+# Modularity is the ability to break a program into smaller pieces that are easier to understand and maintain.
