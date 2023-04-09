4cf28f58159e61657678ab77598de4ae53ce5cf0
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2023-04-09 19:47:36 -0300

    feat: functions for more clarity

--- Projects/hangman_game.py
+++ Projects/hangman_game.py
@@ -1,42 +1,142 @@
 import random
 
-def play():
+def show_welcome_message():
     print("*********************************************")
     print("**************** Hangman Game ***************")
     print("*********************************************")
-    
-    file = open("words.txt", "r")
+
+def load_secret_word():
     words = []
     
-    for line in file:
-        words.append(line.strip())
-    
-    file.close()
+    with open("words.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            words.append(line)
     
     number = random.randrange(0, len(words))
-
     secret_word = words[number].upper()
+    return secret_word
+
+def load_correct_letters(secret_word):
     correct_letters = ["_" for letter in secret_word]
-        
+    return correct_letters
+
+def ask_guess():
+    guess = input("What is your guess? ")
+    guess = guess.strip().upper() # Remove whitespace from the beginning and end of the string, and convert to uppercase.
+    return guess
+
+def set_correct_letters(guess, correct_letters, secret_word):
+    index = 0
+    for letter in secret_word:
+        if letter == guess:
+            correct_letters[index] = letter
+        index += 1
+
+def winner_message():
+    print("You guessed it!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+    print("Congratulations! You won!")
+
+def loser_message(secret_word):
+    print("You got hanged!")
+    print("The secret word was {}".format(secret_word))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def draw_hangman(tries):
+    print("  _______     ")
+    print(" |/      |    ")
+    
+    if tries == 6:
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+    
+    if tries == 5:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+    
+    if tries == 4:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+    
+    if tries == 3:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+    
+    if tries == 2:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+    
+    if tries == 1:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+    
+    if tries == 0:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+    
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+def play():
+    
+    show_welcome_message()
+    secret_word = load_secret_word()
+    correct_letters = load_correct_letters(secret_word)
+    print (correct_letters)
+    
     hanged = False
     guessed = False
     tries = 6
-    
-    print (correct_letters)
-    
+
     while (not hanged and not guessed):
         
-        guess = input("What is your guess? ")
-        guess = guess.strip().upper() # Remove whitespace from the beginning and end of the string, and convert to uppercase.
-        
+        guess = ask_guess()
+                
         if guess in secret_word:
-            index = 0
-            for letter in secret_word:
-                if letter == guess:
-                    correct_letters[index] = letter
-                index += 1
+            set_correct_letters(guess, correct_letters, secret_word)
         else:
             tries -= 1
+            draw_hangman(tries)
             print("You missed! You have {} tries left.".format(tries))
             
         hanged = tries == 0
@@ -44,11 +144,10 @@ def play():
         print(correct_letters)
     
     if guessed:
-        print("You guessed it!")
+        winner_message()
     else:
-        print("You got hanged!")
+        loser_message(secret_word)
     print("Game over!")
-
 
 if __name__ == "__main__":
     play()
