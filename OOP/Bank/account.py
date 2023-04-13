04a6905edf04f73850eb96b1a2f949fdbd503fe0
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2023-04-13 14:56:15 -0300

    feat: methods

--- OOP/Bank/account.py
+++ OOP/Bank/account.py
@@ -1,6 +1,18 @@
+
+
 class Account:
+    
     def __init__(self, number, holder, balance, limit):
         self.number = number
         self.holder = holder
         self.balance = balance
         self.limit = limit
+        
+    def extract(self):
+        print(f"Account number: {self.number}, Holder: {self.holder}, Balance: {self.balance}, Limit: {self.limit}")
+        
+    def deposit(self, value):
+        self.balance += value
+        
+    def withdraw(self, value):
+        self.balance -= value
--- OOP/Bank/test_account.py
+++ OOP/Bank/test_account.py
@@ -1,4 +1,13 @@
 from account import Account
 
 account = Account(123, "Felipe", 55.0, 1000.0)
-print (account.balance)
+account.extract()
+
+account2 = Account(321, "Luis", 100.0, 1000.0)
+account2.extract()
+
+account.deposit(50.0)
+account.extract()
+
+account2.withdraw(10.0)
+account2.extract()
